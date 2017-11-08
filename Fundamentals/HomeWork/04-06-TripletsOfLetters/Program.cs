a40bf0b36a288a0ae554404217f57baaf09f647d
iozzilina <iova_dineva@yahoo.com>
2017-11-08 17:26:26 +0200

    03 html and css hw

---
+++ Fundamentals/HomeWork/04-06-TripletsOfLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_06_TripletsOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Console.WriteLine($"{(char)('a'+i)}{(char)('a' + j)}{(char)('a' + k)}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/HomeWork/04e-19-TheaThePhotographerTimeSpan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace _04e_19_TheaThePhotographerTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPictures = Int32.Parse(Console.ReadLine());
+            int timeToFilter = Int32.Parse(Console.ReadLine()); //in seconds
+            int filterFactor = Int32.Parse(Console.ReadLine()); //percentage of good photos
+            int timeToUpload = Int32.Parse(Console.ReadLine()); //in seconds
+            
+            int numberOfGoodPictures = (int)Math.Ceiling(numberOfPictures * filterFactor / 100m);
+            
+            TimeSpan time = new TimeSpan(0,0,0,numberOfPictures * timeToFilter + numberOfGoodPictures * timeToUpload);
+
+            Console.WriteLine(time.ToString("d:hh:mm:ss"));
+        }
+    }
+}
